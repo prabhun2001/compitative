e27533c45c1803e79f58022db5f77800fd60b07c
PRABHU N <69341981+prabhun2001@users.noreply.github.com>
2021-12-02 21:44:27 +0530

    Create binaryarray_maxlen_substring.cpp
    subarray should contain an equal number of 0s and 1s 
    half the subarray should be 1s and another half should be 0s

---
+++ binaryarray_maxlen_substring.cpp
@@ -0,0 +1,47 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int fun(vector<int> v){
+    int max_len=0;
+    int zero_len=0,total_zero=0;
+    int curr_len=0;
+    int one_len=0,total_one=0;
+    int n=v.size();
+    for(int i=0;i<n;i++){
+        if(v[i]==0 && v[i-1]==1){
+            total_one=one_len;
+            one_len=0;
+        }
+        if(v[i]==1 && v[i-1]==0){
+            total_zero=zero_len;
+            zero_len=0;
+        }
+        if(v[i]==0){
+            
+            zero_len+=1;
+            
+        }
+        if(v[i]==1){
+            
+            one_len+=1;
+           
+        }
+        if(total_zero < total_one){
+            curr_len=2*total_zero;
+        }else{
+            curr_len=2*total_one;
+        }
+        if(max_len<(curr_len)){
+            max_len=(curr_len);
+        }
+    }
+   return max_len;
+}
+
+int main(){
+    int n;
+    vector<int> v{0,1,1,0,0,1,1,1,0,1};
+    n=fun(v);
+    cout<<"\nlength = "<<n;
+    return 0;
+}

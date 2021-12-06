ef1a667fc2e3a3b8539abe429cdbf49b562f311d
PRABHU N <69341981+prabhun2001@users.noreply.github.com>
2021-12-06 22:03:25 +0530

    Create SieveOfEratosthenes.cpp

---
+++ SieveOfEratosthenes.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    vector<int> prime;
+    int n;
+    cout<<"Enter n : ";
+    cin>>n;
+    for(int i=0;i<=n;i++){
+        prime.push_back(1);//cinsider all values as prime
+    }
+    prime[0]=0;
+    prime[1]=0;
+    for(int i=2;i<=n;i++){
+        if(prime[i]==1){
+            for(int j=2;i*j<=n;j++){
+                prime[i*j]=0;
+            }
+        }
+    }
+    for(int i=2;i<=n;i++){
+        if(prime[i]==1){
+            cout<<i<<" ";
+        }
+    }
+}
